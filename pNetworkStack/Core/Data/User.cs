662b2dbfb5320162e32206cfbf07491bba1182cd
Aaron Knoop <aaron.knoop@live.nl>
2020-11-20 16:55:24 +0100

    Added basic user data

---
+++ pNetworkStack/Core/Data/User.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace pNetworkStack.Core.Data
+{
+	public class User
+	{
+		public string UUID;
+		public string Username;
+
+		public Action<pVector> OnPositionUpdated, OnEulerUpdated;
+
+		private pVector m_Position, m_Euler;
+
+		public User()
+		{
+			m_Position = pVector.Zero();
+			m_Euler = pVector.Zero();
+		}
+		
+		public pVector GetPosition()
+		{
+			return m_Position;
+		}
+
+		public pVector GetEuler()
+		{
+			return m_Euler;
+		}
+
+		internal void UpdatePosition(pVector pos)
+		{
+			m_Position = pos;
+			OnPositionUpdated?.Invoke(pos);
+		}
+
+		internal void UpdateEuler(pVector euler)
+		{
+			m_Euler = euler;
+			OnEulerUpdated?.Invoke(euler);
+		}
+	}
+}
