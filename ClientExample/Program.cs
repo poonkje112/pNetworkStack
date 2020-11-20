3bd438edc0feb8c8a79a444be7f0bde44f38a036
Aaron Knoop <aaron.knoop@live.nl>
2020-11-20 16:57:38 +0100

    Added userjoined and left events

--- ClientExample/Program.cs
+++ ClientExample/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using pNetworkStack.client;
 using pNetworkStack.Core;
+using pNetworkStack.Core.Data;
 
 namespace ClientExample
 {
 	class Program
 	{
+		static Dictionary<string, User> ConnectedUsers = new Dictionary<string, User>();
 		private static void Main(string[] args)
 		{
 			InitializeDebugger();
+			RegisterEvents();
 
-			Client client = new Client( "127.0.0.1", 4007);
+			Console.Write("Username: ");
+			string username = Console.ReadLine();
+
+			Client.Username = username;
+			
+			Client client = Client.CreateClient( "127.0.0.1", 4007);
 
 			while (client.IsConnected)
 			{
@@ -19,6 +28,7 @@ namespace ClientExample
 				if(string.IsNullOrEmpty(message)) continue;
 				
 				Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+				Console.WriteLine();
 				client.Send(message);
 				
 				if (message.ToLower() == "exit")
@@ -28,6 +38,26 @@ namespace ClientExample
 			}
 			
 			Console.ReadKey();
+		}
+
+		public static void RegisterEvents()
+		{
+			Client.OnMessageReceived += (user, message) =>
+			{
+				Console.WriteLine($"{user.Username}: {message}");
+			};
+
+			Client.OnUserJoined += user =>
+			{
+				Console.WriteLine($"{user.Username} has joined the game!");
+				ConnectedUsers.Add(user.UUID, user);
+			};
+			
+			Client.OnUserLeft += uid =>
+			{
+				Console.WriteLine($"{ConnectedUsers[uid].Username} has left the game!");
+				ConnectedUsers.Remove(uid);
+			};
 		}
 		
 		private static void InitializeDebugger()
