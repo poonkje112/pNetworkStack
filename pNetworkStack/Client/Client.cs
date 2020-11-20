8d52d823953ad454f095b9fde3bed257aa63a40c
Aaron Knoop <aaron.knoop@live.nl>
2020-11-20 16:56:37 +0100

    Refactoring and added the ability to disconnect a client

--- pNetworkStack/Client/Client.cs
+++ pNetworkStack/Client/Client.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -10,32 +11,70 @@ namespace pNetworkStack.client
 {
 	public class Client
 	{
-		//TODO Parse the received data to the CommandHandler
+		private static Client m_Instance;
 
-		private readonly Socket m_Client;
+		public static string Username;
 
-		public Client(string ip, int port)
+		private Socket m_Client;
+
+		public static Action OnClientConnected, OnClientDisconnected;
+		public static Action<User, string> OnMessageReceived;
+		public static Action<string> OnUserLeft;
+		public static Action<User> OnUserJoined;
+
+		internal Dictionary<string, User> ConnectedUsers = new Dictionary<string, User>();
+
+		public User OurUser;
+
+		/// <summary>
+		/// This will create and return a new client connection instance
+		/// </summary>
+		/// <param name="ip">target ip for connection</param>
+		/// <param name="port">target port for connection</param>
+		/// <returns>A new running instance</returns>
+		public static Client CreateClient(string ip, int port)
+		{ 
+			if(m_Instance == null) return new Client(ip, port);
+			return m_Instance;
+		}
+
+		private Client(string ip, int port)
 		{
-			// Parse the ip
-			IPAddress target = IPAddress.Parse(ip);
-			IPEndPoint remoteEndPoint = new IPEndPoint(target, port);
-
-			try
+			if (m_Instance == null)
 			{
-				// Create the socket
-				m_Client = new Socket(target.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				// Parse the ip
+				IPAddress target = IPAddress.Parse(ip);
+				IPEndPoint remoteEndPoint = new IPEndPoint(target, port);
 
-				// Connect to the server
-				m_Client.Connect(remoteEndPoint);
-			}
-			catch (SocketException e)
-			{
-				Debugger.Log(e.Message, LogType.Error);
-				return;
-			}
+				if (OurUser == null) OurUser = new User();
 
-			// Start the receive loop
-			Receive(m_Client);
+				OurUser.Username = Username;
+
+				try
+				{
+					// Create the socket
+					m_Client = new Socket(target.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+					m_Instance = this;
+
+					// Connect to the server
+					m_Client.Connect(remoteEndPoint);
+					OnClientConnected?.Invoke();
+				}
+				catch (SocketException e)
+				{
+					m_Instance = null;
+					Debugger.Log(e.Message, LogType.Error);
+					return;
+				}
+
+				// Start the receive loop
+				Receive(m_Client);
+			}
+		}
+
+		public static Client GetCurrent()
+		{
+			return m_Instance;
 		}
 
 		private void Receive(Socket client)
@@ -61,36 +100,46 @@ namespace pNetworkStack.client
 		{
 			// Retrieve our ClientData from our async object
 			ClientData data = (ClientData) ar.AsyncState;
-			Socket handler = data.WorkClient;
 
-			// Get the amount of data
-			int bytesToRead = handler.EndReceive(ar);
-
-			// Check if there is any data to process
-			if (bytesToRead > 0)
+			try
 			{
-				// Apply the received data to the string builder
-				data.Builder.Append(Encoding.ASCII.GetString(data.Buffer, 0, bytesToRead));
+				Socket handler = data.WorkClient;
 
-				// Check if we have reached the end
-				string content = data.Builder.ToString();
-				if (content.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+				// Get the amount of data
+				int bytesToRead = handler.EndReceive(ar);
+
+				// Check if there is any data to process
+				if (bytesToRead > 0)
 				{
-					// Parse the command to the parser
-					Util.ParseCommand(content.Replace("<EOF>", ""),
-						(command, parameters) =>
-						{
-							CommandHandler.GetHandler().ExecuteClientCommand(command, parameters);
-						});
+					// Apply the received data to the string builder
+					data.Builder.Append(Encoding.ASCII.GetString(data.Buffer, 0, bytesToRead));
 
-					// Clear the buffer and builder to prepare for new data
-					data.Buffer = new byte[ClientData.BufferSize];
-					data.Builder.Clear();
+					// Check if we have reached the end
+					string content = data.Builder.ToString();
+					if (content.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+					{
+						// Parse the command to the parser
+						Util.ParseCommand(content.Replace("<EOF>", ""),
+							(command, parameters) =>
+							{
+								CommandHandler.GetHandler().ExecuteClientCommand(command, parameters);
+							});
+
+						// Clear the buffer and builder to prepare for new data
+						data.Buffer = new byte[ClientData.BufferSize];
+						data.Builder.Clear();
+					}
 				}
-			}
 
-			// Start receiving again
-			handler.BeginReceive(data.Buffer, 0, ClientData.BufferSize, 0, ReceiveCallback, data);
+				// Start receiving again
+				handler.BeginReceive(data.Buffer, 0, ClientData.BufferSize, 0, ReceiveCallback, data);
+			}
+			catch (SocketException e)
+			{
+				// Assume the serer has stopped
+				Debugger.Log("Lost connection!", LogType.Warning);
+				StopConnection();
+			}
 		}
 
 		/// <summary>
@@ -104,6 +153,29 @@ namespace pNetworkStack.client
 			byte[] data = Encoding.ASCII.GetBytes(message + "<EOF>");
 
 			m_Client.Send(data, 0, data.Length, 0);
+		}
+
+		/// <summary>
+		/// Stops the current client connection and destroys the running instance
+		/// </summary>
+		public void StopConnection()
+		{
+			if (m_Client.Connected)
+			{
+				try
+				{
+					m_Client.Disconnect(false);
+				}
+				catch (Exception e)
+				{
+					// ignored
+				}
+
+				m_Client = null;
+				m_Instance = null;
+
+				OnClientDisconnected?.Invoke();
+			}
 		}
 
 		/// <summary>
