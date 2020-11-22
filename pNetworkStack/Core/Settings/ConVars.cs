44ff927ff3d18cce8f0007f26c727a318b02c5db
Aaron Knoop <aaron.knoop@live.nl>
2020-11-22 13:09:23 +0100

    Renamed TickSpeed to TicksPerSecond

--- pNetworkStack/Core/Settings/ConVars.cs
+++ pNetworkStack/Core/Settings/ConVars.cs
@@ -2,7 +2,7 @@ namespace pNetworkStack.Core.Settings
 {
 	public static class ConVars
 	{
-		public static int TickSpeed = 64;
+		public static int TicksPerSecond = 64;
 		public static string ip;
 		public static string port;
 		public static string rcon_password;
